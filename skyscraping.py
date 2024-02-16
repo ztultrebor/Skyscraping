c11f591739d22cd40e3ddffda6d13fb34d638fed
Robert Lutz <robertlutzi@gmail.com>
2024-02-15 21:27:09 -0500

    refactor into header function

--- skyscraping.py
+++ skyscraping.py
@@ -9,25 +9,33 @@ from bs4 import BeautifulSoup
 
 def get_page(url):
     """String -> String
-        Takes a web address and a parser and returns the parse tree of that web page"""
+        Takes a web address and returns the page data of that web page"""
     return requests.get(url).text
 
 
 def parse(page, parser='html.parser'):
     """String String -> String
-        Takes a web address and a parser and returns the parse tree of that web page"""    
+        Takes a web page data string and a parser and
+        returns the parse tree of that web page"""
     return BeautifulSoup(page, parser)
+
+
+def get_headers(tree):
+    """String -> [ListOf String]
+        Takes a parse tree for a basketball-reference advanced player stats page 
+        and returns a list of column headers for that page"""
+    return [s.text for s in tree.tr.find_all('th')]
 
 
 
 #=====================
-# 
+# action!
 
 
 br_advanced_2024 = 'https://www.basketball-reference.com/leagues/NBA_2024_advanced.html'
 
 page_parse_tree = parse(get_page(br_advanced_2024))
 
-headers = [s.text for s in page_parse_tree.tr.find_all('th')]
+headers = get_headers(page_parse_tree)
 
 print(headers)
