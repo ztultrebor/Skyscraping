bbe166789ee9c46ebde59dbe9d43a1c93153a3da
Robert Lutz <robertlutzi@gmail.com>
2024-02-15 21:21:16 -0500

    extract column headers

--- skyscraping.py
+++ skyscraping.py
@@ -2,14 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_page(url, parser):
-    """String String -> String
-        Takes a web address and a parser and returns the parse tree of that web page"""
-    content = requests.get(url).text
-    return BeautifulSoup(content, parser)
 
+#=========================
+ # functions
+
+
+def get_page(url):
+    """String -> String
+        Takes a web address and a parser and returns the parse tree of that web page"""
+    return requests.get(url).text
+
+
+def parse(page, parser='html.parser'):
+    """String String -> String
+        Takes a web address and a parser and returns the parse tree of that web page"""    
+    return BeautifulSoup(page, parser)
+
+
+
+#=====================
+# 
 
 
 br_advanced_2024 = 'https://www.basketball-reference.com/leagues/NBA_2024_advanced.html'
 
-print(get_page(br_advanced_2024, "html.parser"))
+page_parse_tree = parse(get_page(br_advanced_2024))
+
+headers = [s.text for s in page_parse_tree.tr.find_all('th')]
+
+print(headers)
